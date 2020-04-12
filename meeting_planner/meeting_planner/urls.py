e17da6fc6c33bc3316acad8bb5ee0ce6e560d013
Justin Miller <indymiller@gmail.com>
2020-04-12 13:57:19 -0400

    welcome and date sites added

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import welcome
+from website.views import date
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome.html', welcome),
+    path('date.html', date)
 ]
