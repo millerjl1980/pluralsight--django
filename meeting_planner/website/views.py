80da85b5a497787922caed81e4832544185d5cf6
Justin Miller <indymiller@gmail.com>
2020-04-16 15:32:19 -0400

    added website template

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to the Meeting Planner!")
+    return render(request, 'website/welcome.html',
+                   {'message': 'This data was sent from the view to the template'})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
