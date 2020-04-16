d3cd3b651dc62fe3c271c5497d67081ae8b8ca01
Justin Miller <indymiller@gmail.com>
2020-04-16 15:58:42 -0400

    added details page with 404 handeling

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
     return render(request, 'website/welcome.html',
-                   {'message': 'This data was sent from the view to the template'})
+                   {'message': 'This data was sent from the view to the template',
+                   'num_meetings': Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
