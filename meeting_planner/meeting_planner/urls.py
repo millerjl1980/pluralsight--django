69df231dcacafd85b7bcb7482c14923984375937
Justin Miller <indymiller@gmail.com>
2020-04-18 16:06:00 -0400

    added url mapping best practices

--- meeting_planner/meeting_planner/urls.py
+++ meeting_planner/meeting_planner/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from website.views import welcome, date, about
-from meetings.views import detail, rooms_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name='welcome'),
     path('date.html', date),
     path('about.html', about),
-    path('meetings/<int:id>', detail, name='detail'),
-    path('rooms', rooms_list, name='rooms'),
+    path('meetings/', include('meetings.urls')),
 ]
