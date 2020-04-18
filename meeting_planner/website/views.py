6dbb0c0a2caf8fa4914d472510ef66d07e90e366
Justin Miller <indymiller@gmail.com>
2020-04-18 13:40:18 -0400

    list of meetings added to front page

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -7,7 +7,7 @@ from meetings.models import Meeting
 def welcome(request):
     return render(request, 'website/welcome.html',
                    {'message': 'This data was sent from the view to the template',
-                   'num_meetings': Meeting.objects.count()})
+                   'meetings': Meeting.objects.all()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
